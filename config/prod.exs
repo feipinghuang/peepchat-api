93866236f118a9eca12f114ae9f0c0301deda749
feipinghuang <feiping.huang@thexstore.cn>
2016-07-03 18:44:33 +0800

    add config/prod.secret.exs back

--- config/prod.exs
+++ config/prod.exs
@@ -14,8 +14,7 @@ use Mix.Config
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [host: "shielded-escarpment-48448.herokuapp.com", port: 443],
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  url: [host: "shielded-escarpment-48448.herokuapp.com", port: 443]
 
 config :peepchat, Peepchat.Repo,
   adapter: Ecto.Adapters.Postgres,
