b4c1e98f55db0bb73991c93f4650cc569b520616
feipinghuang <feiping.huang@thexstore.cn>
2016-07-03 18:39:24 +0800

    fix config bug

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "shielded-escarpment-48448.herokuapp.com", port: 443],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :peepchat, Peepchat.Repo,
   adapter: Ecto.Adapters.Postgres,
