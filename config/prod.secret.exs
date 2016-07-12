b0ccbdb5e0b7119c05fb512be24688e7d6f351a7
feipinghuang <feiping.huang@thexstore.cn>
2016-07-12 10:37:00 +0800

    fix 503

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -9,11 +9,13 @@ use Mix.Config
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or you later on).
 config :peepchat, Peepchat.Endpoint,
+  http: [port: {:system, "PORT"}],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "shielded-escarpment-48448.herokuapp.com", port: 443],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :peepchat, Peepchat.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("DATABASE_USERNAME"),
-  password: System.get_env("DATABASE_PASSWORD"),
+  url: System.get_env("DATABASE_URL"),
   pool_size: 20
